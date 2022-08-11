fc56689d2156a717cee52c3d257d4989bb93577a
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-08-11 16:17:24 +0100

    I have completed the javascript and structure for the grid gallery.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,15 @@
     "use strict";
 
     $(document).ready(function() {
-
+        // $('.gallery-container').magnificPopup({
+        //     delegate: 'a', // child items selector, by clicking on it popup will open
+        //     type: 'image',
+        //     arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+        //     tPrev: 'Previous (Left arrow key)', // title for left button
+        //     tNext: 'Next (Right arrow key)', // title for right button
+        //     tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+        //     // other options
+        // });
 
         /* ==================================================
             # Wow Init
